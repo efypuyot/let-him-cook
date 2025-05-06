9760f05be2dfee0ce9105acb9e29615786c94ecc
ellen <eypuyot@up.edu.ph>
2025-05-06 08:02:57 +0800

    [fe] add bg

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,10 +13,30 @@ export default {
 
 <style>
 #app {
+  position: relative;
+  z-index: 1;
+  min-height: 100vh;
   font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  overflow: hidden;
+}
+
+/* Background layer with opacity */
+#app::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('@/assets/background.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.03; /* very low opacity */
+  z-index: -1; /* put it behind the app content */
 }
 </style>
